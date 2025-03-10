c22d03909d36ad6f7c2ee4d0cdf1217924e7e9ac
Aravind-808 <halfwind2005@gmail.com>
2025-03-10 22:46:06 +0530

    organized and moved stuff

--- character-level-lm/trigram/trigram.py
+++ character-level-lm/trigram/trigram.py
@@ -1,3 +1,6 @@
+import os
+import sys
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
 import matplotlib.pyplot as plt
 import torch
 from utils import init_charint_map
